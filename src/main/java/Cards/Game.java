26c0332618172bcb8f149478862704cf1bf33a87
andrei <andrei.666.666@mail.ru>
2021-02-24 21:22:34 +0900

    add: new class Game

---
+++ src/main/java/Cards/Game.java
@@ -0,0 +1,9 @@
+package Cards;
+
+public class Game {
+    private byte player_number;
+
+    public void setPlayer_number(byte player_number) {
+        this.player_number = player_number;
+    }
+}
--- src/main/java/Cards/Main.java
+++ src/main/java/Cards/Main.java
@@ -1,7 +1,20 @@
 package Cards;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        Game someGame = new Game();
+
+        System.out.println("Введите кол-во игроков в диапазоне от 1-6.");
+        byte player_number = input.nextByte();
+        while(player_number < 0 || player_number > 6){
+            System.out.println("Указано неверное кол-во игроков. Повторите ввод.");
+            player_number = input.nextByte();
+        }
+        someGame.setPlayer_number(player_number);
     }
+
 }
