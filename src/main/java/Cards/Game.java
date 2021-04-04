1e08abe69832e3bcb9dba39e49d9422a943658fb
chiki-andrei <andrei.666.666@mail.ru>
2021-04-04 18:29:25 +0900

    fix

--- src/main/java/Cards/Game.java
+++ src/main/java/Cards/Game.java
@@ -2,7 +2,7 @@ package Cards;
 
 public class Game {
     private int player_number;
-    private String[] cardDeck = new String[52];
+    private PlayingCard[] cardDeck = new PlayingCard[52];
 
     public void setPlayer_number(int player_number) {
         this.player_number = player_number;
@@ -12,7 +12,7 @@ public class Game {
         int temp = 0;
         for(int i=0; i<4; i++){
             for(int j=0; j<13; j++){
-                cardDeck[temp++] = new PlayingCard(PlayingCard.SUITS_LIST[i], PlayingCard.RANK_LIST[j]).toString();
+                cardDeck[temp++] = new PlayingCard(PlayingCard.SUITS_LIST[i], PlayingCard.RANK_LIST[j]);
             }
         }
     }
@@ -20,13 +20,12 @@ public class Game {
     public void deckShuffle(){
         int temp1;
         int temp2;
-        String tempString;
         for(int i=0; i<2704; i++){
             temp1 = (int) (Math.random()*52);
             temp2 = (int) (Math.random()*52);
-            tempString = cardDeck[temp1];
+            PlayingCard tempCard = cardDeck[temp1];
             cardDeck[temp1] = cardDeck[temp2];
-            cardDeck[temp2] = tempString;
+            cardDeck[temp2] = tempCard;
         }
     }
 
